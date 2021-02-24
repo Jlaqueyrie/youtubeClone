cde96bad6dce8171ead9f8626be4654dabdcabc0
jlaqueyrie <jerome.laqueyrie@hotmail.com>
2021-02-24 15:16:00 +0100

    ajout header et footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,27 +1,4 @@
-<!doctype html>
-<html lang="en">
 
-<head>
-    <title>Youtube clone</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/styles.css">
-
-</head>
-
-<body>
-    <div class="container" id="pageContainer">
-
-        <header>bouton+loho+barre de recherche+avatar</header>
-        <aside>Menu</aside>
-        <section id="mainSectionContainer">
-            <div class="mainContent">Vidéo
-            
             </div>
 
         </section>
