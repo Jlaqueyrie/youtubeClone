20eb0dba49b3aad54b2222a892faea560d359928
jlaqueyrie <jerome.laqueyrie@hotmail.com>
2021-02-24 16:24:10 +0100

    positionnement index  ok

--- includes/header.php
+++ includes/header.php
@@ -21,27 +21,26 @@
             <button class="navShowHide">
                 <img src="assets/images/icons/menu.png" alt="menu">
             </button>
-        <a href="index.php" class="logoContainer">
-            <img src="assets/images/logo-youtube.png" alt="logo">
-        </a>
-        <div class="searchBarContainer">
-            <form action="search.php" method="GET">
-                <input type="text" name="term" placeholder="Rechercher ..." class="searchBar">
+            <a href="index.php" class="logoContainer">
+                <img src="assets/images/logo-youtube.png" alt="logo">
+            </a>
+            <div class="searchBarContainer">
+                <form action="search.php" method="GET">
+                    <input type="text" name="term" placeholder="Rechercher ..." class="searchBar">
+                    <button class="searchBtn"><img src="assets/images/icons/search.png" alt="search button"></button>
+                </form>
+            </div>
+            
+            <div class="rightIcons">
+                <a href="upload.php">
+                    <img src="assets/images/icons/upload.png" alt="">
+                </a> 
+                <a href="profile.php">
+                    <img src="assets/images/default.png" alt="">
+                </a>
+            </div>
 
-                <button class="searchBtn"><img src="assets/images/icons/search.png" alt="search button">
-                </button>
-            </form>
-        </div>
-        <div class="rightIcons">
-       <a href="upload.php">
-       <img src="assets/images/icons/upload.png" alt=""></a> 
-        </div>
-        <a href="profile.php">
-        
-        
-        <img src="assets/images/default.png" alt="">
-        </a>
-        <aside>Menui</aside>
-        <section id="mainSectionContainer">
+        </header>
+            <aside id="sidenavContainer">Menu</aside>
+            <section id="mainSectionContainer" class="leftPadding">
             <div class="mainContent">Vidéo
-</header>
